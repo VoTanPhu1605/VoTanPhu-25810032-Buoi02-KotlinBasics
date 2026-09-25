ea5fa385aae3a3921e7027b74d71037463c3d974
VoTanPhu1605 <votanphu010@gmail.com>
2026-09-25 15:16:45 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ app/src/main/java/com/example/a25810032_votanphu/Bai02gioithieubanthan.kt
@@ -0,0 +1,16 @@
+package com.example.a25810032_votanphu
+
+// Họ tên: Võ Tấn Phú - MSSV: 25810032
+
+fun main() {
+    val hoTen: String = "Võ Tấn Phú"
+    val namSinh: Int = 2004
+    val namHienTai: Int = 2026
+
+    val tuoi: Int = namHienTai - namSinh
+
+
+    val loiGioiThieu = "Xin chào, tôi tên là $hoTen, sinh năm $namSinh, năm nay tôi $tuoi tuổi."
+
+    println(loiGioiThieu)
+}
