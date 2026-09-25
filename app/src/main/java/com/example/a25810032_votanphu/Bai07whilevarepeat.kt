c6ec70ff6f62d3e75ce6196c00b5f75b63a545fd
VoTanPhu1605 <votanphu010@gmail.com>
2026-09-25 15:18:09 +0700

    Buoi02 Bai07 while va repeat

---
+++ app/src/main/java/com/example/a25810032_votanphu/Bai07whilevarepeat.kt
@@ -0,0 +1,19 @@
+package com.example.a25810032_votanphu
+
+// Họ tên: Võ Tấn Phú - MSSV: 25810032
+
+fun main() {
+    println("Đếm ngược:")
+    var soDem = 10
+    while (soDem >= 1) {
+        println(soDem)
+        soDem--
+    }
+
+    println("In dòng cố định 5 lần:")
+    repeat(5) {
+        println("Xin chào Kotlin!")
+    }
+
+
+}
